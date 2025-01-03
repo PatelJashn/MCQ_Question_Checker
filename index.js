c8e7ad96af705cef61a139ebc2b9c954b384ae24
jashnpatel007 <127716166+jashnpatel007@users.noreply.github.com>
2025-01-03 09:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 document.getElementById("Submitbutton").onclick = function() {
-   // Array of correct answers
    const correctAnswers = ['c', 'a', 'a', 'a', 'a'];
    
    let totalMarks = 0;
 
-   // Loop through each question
    for (let i = 1; i <= correctAnswers.length; i++) {
        const selectedAnswer = document.querySelector(`input[name="lol${i}"]:checked`);
        
-       // Check if the selected answer matches the correct one
        if (selectedAnswer && selectedAnswer.value === correctAnswers[i - 1]) {
            totalMarks += 1;
        }
